2d967afeb1e0682cf1e6b5a00c3492ee746f49a4
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2017-11-30 07:47:45 -0200

    edited .scss

--- docs/js/chart.js
+++ docs/js/chart.js
@@ -3,22 +3,25 @@
 
        type: 'line',
        data: {
-           labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+           labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
            datasets: [{
-               label: "B12",
+               label: " Vit. B12 ",
                borderColor: 'rgb(255, 99, 132)',
+               pointHoverBackgroundColor: 'rgb(255, 99, 132)',
                fill: false,
                data: [0, 10, 5, 2, 20, 30, 45],
            },
         {
-            label: "D",
+            label: " Vit. D ",
             borderColor: 'rgba(36, 249, 203, 1)',
+            pointHoverBackgroundColor: 'rgba(36, 249, 203, 1)',
             fill: false,
             data: [0, 20, 25, 22, 34, 38, 48],
         },
         {
-            label: "C",
+            label: " Vit. C ",
             borderColor: 'rgba(0, 207, 214, 1)',
+            pointHoverBackgroundColor: 'rgba(0, 207, 214, 1)',
             fill: false,
             data: [0, 22, 20, 28, 37, 43, 56],
         }]
